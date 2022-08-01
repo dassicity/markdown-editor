1cb697ae1bbb7233cbd7735acaa9d1ce7426e5f1
Soumyanil Das <dassic@outlook.com>
2022-08-01 11:27:16 +0530

    Added Sharejs and redis dependancy

---
+++ Readme.md
@@ -0,0 +1,2 @@
+# A simple online Markdown Editor
+
--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const share = require('share');
+
+const redis = require('redis');
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -9,6 +12,15 @@ app.use(express.static(__dirname + '/public'));
 app.get('/', (req, res, next) => {
     res.render('pad');
 });
+app.get('/(:id)', (req, res, next) => {
+    res.render('pad');
+});
+
+let options = {
+    db: { type: 'redis' },
+};
+
+share.server.attach(app, options);
 
 const port = process.env.PORT || 8000;
 app.listen(port);
